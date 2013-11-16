2172b297c7a6539095f1e794c5bfa4addcb1184a
Hemant Torsekar <hemant.torsekar@ryatta.com>
2013-11-16 00:13:31 -0500

    Date/time added to menu-bar.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Portfolio::Application.config.secret_key_base = '4533ba917c230c217762564cbb6fbaca4d367889c1f934a17a6f842722e248ed77150414097edca03373be518ca793ce8b1710e3ae1769e70ade63f2c612c9ce'
